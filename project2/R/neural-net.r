a1f284ea03f9979fc731fdb562aa876dc6abe9c4
skarning <sivertskarning@hotmail.com>
2020-03-18 12:13:16 +0100

    changed hyperparameters

--- project2/R/neural-net.r
+++ project2/R/neural-net.r
@@ -1,6 +1,7 @@
 library(caret)
 library(plyr)
 require(neuralnet)
+require(ROCR)
 
 # Import data
 cardata <- readRDS(file = "data/cardata.Rds")
@@ -11,13 +12,11 @@ set.seed(500)   #sets seed to 500 for recreation
 rows <- sample(nrow(cardata))
 randomCardata <- cardata[rows, ]
 
-
 #https://stackoverflow.com/questions/17200114/how-to-split-data-into-training-testing-sets-using-sample-function
 #Split dataset
 split <- sort(sample(nrow(randomCardata), nrow(randomCardata)*.75))
 train <- randomCardata[split,]
 test <- randomCardata[-split,]
-
 
 # One hot encoding
 #https://amunategui.github.io/dummyVar-Walkthrough/
@@ -32,13 +31,11 @@ encodedTrain <- oneHotEncode(train)
 encodedTest <- oneHotEncode(test)
 encodedFull <- oneHotEncode(randomCardata)
 
-
 # Neuralnet prediction
-                                        # Neural network fitting from: https://www.datacamp.com/community/tutorials/neural-network-models-r
-neuralNetModel <- neuralnet("buying.high + buying.low + buying.med + buying.vhigh ~ .", data=encodedTrain, hidden=3, linear.output = FALSE)
+# Neural network fitting from: https://www.datacamp.com/community/tutorials/neural-network-models-r
+neuralNetModel <- neuralnet("buying.high + buying.low + buying.med + buying.vhigh ~ .", data=encodedTrain, hidden=c(5,3), linear.output = FALSE)
 result.mat <- neuralNetModel$result.matrix
 
-plot(neuralNetModel)
 accuracy <- function(encodedData, res, from, to) {
     class <- max.col(encodedData[,from:to])
     predictedClass <- max.col(res)
@@ -50,8 +47,6 @@ predictionTable <- function(model, encodedData) {
     return(compute(model, encodedData)$net.result)
 }
 
-
-
 # Cross validation
 # https://www.r-bloggers.com/multilabel-classification-with-neuralnet-package/
 crossValidate <- function(k, partition, encodedData, from, to) {
@@ -59,8 +54,8 @@ crossValidate <- function(k, partition, encodedData, from, to) {
     for (i in 1:k) {
         split <- sample(1:nrow(encodedData), round(partition*nrow(encodedData)))
         trainPartition <- encodedData[split, ]
-        testPartition <- encodedData[-split, ]
-        currentModel <- neuralnet("buying.high + buying.low + buying.med + buying.vhigh ~ .", data=trainPartition, stepmax = 1e+06, hidden=3, act.fct = "logistic", linear.output = FALSE)
+        testPartition <- encodedData[-split, ]                                         #hidden is the number of hidden layers for specifiyin node use vector c(5,2) ex.
+        currentModel <- neuralnet("buying.high + buying.low + buying.med + buying.vhigh ~ .", data=trainPartition, hidden=5, act.fct = "tanh", linear.output = FALSE)
 
         dataPrediction <- predictionTable(currentModel, testPartition)
         acurracies[i] <- accuracy(testPartition, dataPrediction, from, to)
@@ -78,9 +73,8 @@ testDataPrediction <- predictionTable(neuralNetModel, encodedTest)
 testDataAccuracy <- accuracy(encodedTest, testDataPrediction, 1, 4)
 
 # Cross validation
-crossValidationAccuracy <- crossValidate(10, 0.90, encodedFull, 1, 4)
+# crossValidationAccuracy <- crossValidate(10, 0.90, encodedFull, 1, 4)
 
-predictionResult <- data.frame(c("Training data", "Test data", "Cross validation"), c(trainDataAccuracy, testDataAccuracy, crossValidationAccuracy))
-
-print(crossValidationAccuracy)
-print(predictionResult)
+print(trainDataAccuracy)
+print(testDataAccuracy)
+# print(crossValidationAccuracy)
