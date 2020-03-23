77fef758a62be84870f5d395d795c6715ce9c16d
skarning <sivertskarning@hotmail.com>
2020-03-23 17:30:10 +0100

    Finish learning curve in r

--- project2/R/learning-curve.r
+++ project2/R/learning-curve.r
@@ -1,11 +1,19 @@
 library(caret)
-require(neuralnet)
+library(neuralnet)
+library(ggplot2)
+
+
+# softmax activation function
+# https://en.wikipedia.org/wiki/Softmax_function#Neural_networks
+mySoftMax <- function(value) exp(value)/sum(exp(value))
+
 
 learningCurve <- function(input, res) {
     jump <- floor(nrow(input)/res)
     endIndex <- 0
 
     results <- NULL
+    trainingResults <- NULL
     endIndexes <- NULL
     for (i in 1:res) {
         # limit the data at the end-index
@@ -13,22 +21,29 @@ learningCurve <- function(input, res) {
         limitedData <- input[0:endIndex, ]
         # https://stackoverflow.com/questions/17200114/how-to-split-data-into-training-testing-sets-using-sample-function
         # Split dataset
-        split <- sort(sample(nrow(limitedData), nrow(limitedData)*.75))
+        split <- sort(sample(nrow(limitedData), nrow(limitedData)*.80))
         train <- limitedData[split,]
         test <- limitedData[-split,]
 
         # Neuralnet prediction
         # Neural network fitting from: https://www.datacamp.com/community/tutorials/neural-network-models-r
-        neuralNetModel <- neuralnet("buying.high + buying.low + buying.med + buying.vhigh ~ .", data=train, hidden=3, linear.output = FALSE)
-        result.mat <- neuralNetModel$result.matrix
+        neuralNetModel <- neuralnet("buying.high + buying.low + buying.med + buying.vhigh ~ .", data=train, hidden=4, act.fct = "logistic", linear.output = FALSE)
 
+        # Accuracy on training set
+        trainingDataPrediction <- neuralNetModel$net.result[[1]]
+        trainingAccuracy <- accuracy(train, trainingDataPrediction, 1, 4)
+        trainingResults[i] <- trainingAccuracy
+
+        # Accuracy on validation set
         dataPrediction <- predictionTable(neuralNetModel, test)
         accuracy <- accuracy(test, dataPrediction, 1, 4)
         results[i] <- accuracy
+        # Size of test data, used for y-axis
         endIndexes[i] <- endIndex
     }
-    return(data.frame(accuracy = results, experience = endIndexes))
+    return(data.frame(validationAccuracy = results, trainingAccuracy = trainingResults, experience = endIndexes))
 }
+
 
 # One hot encoding
 # https://amunategui.github.io/dummyVar-Walkthrough/
@@ -61,6 +76,9 @@ randomCardata <- cardata[rows, ]
 encodedRandomCardata <- oneHotEncode(randomCardata)
 
 
-results <- learningCurve(encodedRandomCardata, 10)
+results <- learningCurve(encodedRandomCardata, 50)
 
-ggplot(data=results, mapping=aes(x=experience, y=accuracy)) + geom_line() + labs(title="Learning Curve") + theme_classic()
+print(results)
+
+# Plot the learning curve
+ggplot(data=results, mapping=aes(x=experience)) + geom_line(aes(y = validationAccuracy, color = "validation")) + geom_line(aes(y = trainingAccuracy, color = "training")) + scale_color_manual(name = "Sets", values = c("validation" = "blue", "training" = "red"))
